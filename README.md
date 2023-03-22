f4ffd6dc2c88c8fadf6daa5af15a8e54bbad6cd1
Guan <82428319+s3895776@users.noreply.github.com>
2023-03-22 11:22:36 +1100

    Created eclipse project
    removed the intellij project

--- README.md
+++
@@ -1,8 +0,0 @@
-# CloudComputingAssignment1
-
-Make the html sites and link them up with AWS 
-
-Functionality 
-Accounts
-Some other things 
-AWS
---
+++ awsServicesAssignment1/src/main/java/BucketManagement/UploadObject.java
@@ -0,0 +1,51 @@
+package BucketManagement;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UploadObject {
+
+    public static void main(String[] args) throws IOException {
+        Regions clientRegion = Regions.US_EAST_1;
+        String bucketName = "*** Bucket name ***";//e.g., sxxxxxxx-s3test
+        String stringObjKeyName = "*** String object key name ***";//e.g., sample.txt
+        String fileObjKeyName = "*** File object key name ***";//This part can be empty
+        String fileName = "*** Path to file to upload ***";//e.g., sample.txt
+
+        try {
+            //This code expects that you have AWS credentials set up per:
+            // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .build();
+
+            // Upload a text string as a new object.
+            s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+    }
+}
+
